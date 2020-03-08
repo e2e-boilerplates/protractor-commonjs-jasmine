58daadf18f4dbc301371c9e03829891ecf7b921f
Girma Nigusse <xgirma@gmail.com>
2020-03-07 21:30:40 -0500

    update

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,12 +1,25 @@
-exports.config = {
-  directConnect: true,
-  specs: ["spec/*.spec.js"],
+const directConnect = true;
+const specs = ["spec/*.spec.js"];
+
+const headed = {
+  directConnect,
+  specs,
   capabilities: {
     browserName: "chrome"
-  },
-  framework: "jasmine",
-  jasmineNodeOpts: {
-    isVerbose: true,
-    realtimeFailure: true
   }
 };
+
+const headless = {
+  directConnect,
+  specs,
+  capabilities: {
+    browserName: "chrome",
+    chromeOptions: {
+      args: ["--headless", "--no-sandbox", "--disable-gpu"]
+    }
+  }
+};
+
+const config = process.env.GITHUB_ACTIONS ? headless : headed;
+
+exports.config = config;
